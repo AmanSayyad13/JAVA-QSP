b978d651f507f90355ab4d525881f7ffffbb83fd
Saif Sayyad <aaamansayyad.131@gmail.com>
2026-06-28 11:53:01 +0530

    Local Variable

---
+++ DAY 2/LocalVariable.java
@@ -0,0 +1,10 @@
+public class LocalVariable {
+    public static void main(String[] args) {
+        // Declaring and initializing local variables
+        int speed = 60;
+        String driverName = "Alex";
+
+        // Accessing local variables within this method
+        System.out.println(driverName + " is driving at " + speed + " mph.");
+    }
+}
